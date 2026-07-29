b047c0e9ef77319c5983d53b5562b5116962d108
Hari <harivenkat1004@gmail.com>
2026-07-29 12:17:26 +0530

    Added browser launch debugging

--- hooks/hooks.js
+++ hooks/hooks.js
@@ -4,23 +4,32 @@ import { chromium } from "@playwright/test";
 setDefaultTimeout(60000);
 
 Before(async function () {
+    try {
+        console.log("Browser Opening...");
 
-    console.log("Browser Opening...");
+        this.browser = await chromium.launch({
+            headless: true
+        });
 
-    this.browser = await chromium.launch({
-        headless: false
-    });
+        console.log("Browser launched successfully");
 
-    this.page = await this.browser.newPage();
+        this.page = await this.browser.newPage();
 
-    console.log("Browser Opened");
+        console.log("New page created");
+
+    } catch (err) {
+        console.error("Launch Error:", err);
+        throw err;
+    }
 });
 
 After(async function () {
-
     console.log("Closing Browser...");
 
-    await this.browser.close();
-
-    console.log("Browser Closed");
+    if (this.browser) {
+        await this.browser.close();
+        console.log("Browser Closed");
+    } else {
+        console.log("Browser was not created");
+    }
 });
